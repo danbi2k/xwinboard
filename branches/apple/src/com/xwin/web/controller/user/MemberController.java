34639abe3608604afd59b350aee69984b92ed6b5
jongil.choi <jongil.choi@c6210951-4255-0410-9d56-95eb967ad95f>
2009-03-06 14:23:01 +0000

    event expire

--- branches/apple/src/com/xwin/web/controller/user/MemberController.java
+++ branches/apple/src/com/xwin/web/controller/user/MemberController.java
@@ -99,9 +99,11 @@ public class MemberController extends XwinController implements MessageSourceAwa
 											Integer cnt = memberDao.confirmGetJoinEvent(member.getMobile());
 											if (cnt == 0) {
 												//memberDao.loggingGetEvent(member.getMobile());
-												member.setBalance(5000L);
-												member.setJoinBonus(5000);
-												WelcomeMsg = "가입이벤트로 5,000캐쉬가 충전 되었습니다";
+												//member.setBalance(5000L);
+												//member.setJoinBonus(5000);
+												//WelcomeMsg = "가입이벤트로 5,000캐쉬가 충전 되었습니다";
+												member.setBalance(0L);
+												member.setJoinBonus(0);
 												
 											} else {
 												member.setBalance(30000L);
