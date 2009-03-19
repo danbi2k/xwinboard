96f9a2df246a81c00f4db3a7079dc7ab1836148e
jongil.choi <jongil.choi@c6210951-4255-0410-9d56-95eb967ad95f>
2009-03-19 16:25:58 +0000

    apple : game sync by IP

--- src/com/xwin/infra/schedule/SendSmsManager.java
+++ src/com/xwin/infra/schedule/SendSmsManager.java
@@ -13,8 +13,8 @@ import com.xwin.infra.sms.SendSmsConnector;
 
 public class SendSmsManager extends QuartzJobBean
 {
-//	private static final String SERVER_URL = "http://bethit.net:10180/external.aspx?mode=getSmsWaitList";
-//	private static final String ALLUSER_URL = "http://bethit.net:10180/external.aspx?mode=getAllNormalPhoneNumber";
+//	private static final String SERVER_URL = "http://apbet.net/external.aspx?mode=getSmsWaitList";
+//	private static final String ALLUSER_URL = "http://apbet.net/external.aspx?mode=getAllNormalPhoneNumber";
 //	private static final String SERVER_URL = "http://localhost:8080/xwin/external.aspx?mode=getSmsWaitList";
 //	private static final String ALLUSER_URL = "http://localhost:8080/xwin/external.aspx?mode=getAllNormalPhoneNumber";
 	
