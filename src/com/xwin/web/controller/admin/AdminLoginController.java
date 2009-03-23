904d5d26212382e34c18739ae83278caf216ba4f
jongil.choi <jongil.choi@c6210951-4255-0410-9d56-95eb967ad95f>
2009-03-23 10:13:46 +0000

    apple : defence hacking

--- src/com/xwin/web/controller/admin/AdminLoginController.java
+++ src/com/xwin/web/controller/admin/AdminLoginController.java
@@ -1,6 +1,8 @@
 package com.xwin.web.controller.admin;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,11 +14,14 @@ import com.xwin.domain.admin.Access;
 import com.xwin.domain.user.Member;
 import com.xwin.infra.util.Code;
 import com.xwin.infra.util.XmlUtil;
+import com.xwin.infra.util.XwinUtil;
 import com.xwin.web.command.ResultXml;
 import com.xwin.web.controller.XwinController;
 
 public class AdminLoginController extends XwinController
 {
+	private static Map<String, Integer> TRY_IP = new HashMap<String, Integer>();
+	
 	public ModelAndView processLogin(HttpServletRequest request, 
 			HttpServletResponse response) throws Exception
 	{
@@ -26,20 +31,27 @@ public class AdminLoginController extends XwinController
 		String pin = request.getParameter("pin");
 		
 		Member admin = memberDao.selectMember(userId, Code.USER_GRADE_ADMIN);
-		
-		if (admin!= null && admin.getPassword().equals(password) && admin.getPin().equals(pin)) {
-			request.getSession().setAttribute("Admin", admin);
-			rx = ResultXml.SUCCESS;
-			
-			Access access = new Access();
-			access.setDate(new Date());
-			access.setUserId(admin.getUserId());
-			access.setNickName(admin.getNickName());
-			access.setIpAddress(request.getRemoteAddr());
-			access.setType(Code.ACCESS_ADMIN_LOGIN);
-			
-			accessDao.insertAccess(access);
-		} else {
+		String ip = request.getRemoteAddr();
+		TRY_IP.put(ip, XwinUtil.ntz(TRY_IP.get(ip))+1);			
+		Integer tryCount = TRY_IP.get(ip);
+		if (tryCount > 5) {
+			rx = new ResultXml(-1, "로그인 시도 횟수를 초과 하였습니다. 접속을 차단 합니다.", null);
+		} 
+		else if (admin!= null && admin.getPassword().equals(password) && admin.getPin().equals(pin)) {
+				TRY_IP.remove(ip);
+				request.getSession().setAttribute("Admin", admin);
+				rx = ResultXml.SUCCESS;
+				
+				Access access = new Access();
+				access.setDate(new Date());
+				access.setUserId(admin.getUserId());
+				access.setNickName(admin.getNickName());
+				access.setIpAddress(request.getRemoteAddr());
+				access.setType(Code.ACCESS_ADMIN_LOGIN);
+				
+				accessDao.insertAccess(access);
+		}
+		else {
 			rx = new ResultXml(-1, "관리자 정보가 잘못되었습니다", null);
 		}
 		
