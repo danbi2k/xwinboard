e36d1ee8f3357b845aed45f4fd27878fcbbe9dce
jongil.choi <jongil.choi@c6210951-4255-0410-9d56-95eb967ad95f>
2009-05-11 23:52:21 +0000

    bravo : betkr

--- trunk/src/com/xwin/domain/SiteConfig.java
+++ trunk/src/com/xwin/domain/SiteConfig.java
@@ -7,8 +7,8 @@ public class SiteConfig extends XwinObject
 	private static final long serialVersionUID = -3188943696296998618L;
 
 	public static final Locale SITE_LOCALE = Locale.US;	
-	public static final String SITE_NAME = "Bravo";
-	public static final String SITE_DOMAIN = "bravotime.net";
+	public static final String SITE_NAME = "BETKR";
+	public static final String SITE_DOMAIN = "betkr.net";
 	public static final boolean PIN_LOGIN = true;	
 	public static final String SITE_PHONE = "0000000000";
 }
