d2ebe0075cfb0c58a87f57d14136324f95628300
jongil.choi <jongil.choi@c6210951-4255-0410-9d56-95eb967ad95f>
2009-05-25 12:17:09 +0000

    apple : auto star out

--- branches/apple/src/com/xwin/infra/schedule/GameBetStatusManager.java
+++ branches/apple/src/com/xwin/infra/schedule/GameBetStatusManager.java
@@ -2,12 +2,22 @@ package com.xwin.infra.schedule;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import com.xwin.domain.admin.Account;
+import com.xwin.domain.user.Member;
+import com.xwin.domain.user.MoneyOut;
+import com.xwin.infra.dao.AccountDao;
 import com.xwin.infra.dao.GameDao;
+import com.xwin.infra.dao.MemberDao;
+import com.xwin.infra.dao.MoneyOutDao;
+import com.xwin.infra.util.Code;
 
 public class GameBetStatusManager extends QuartzJobBean
 {
@@ -16,10 +26,65 @@ public class GameBetStatusManager extends QuartzJobBean
 			throws JobExecutionException
 	{
 		GameDao gameDao = (GameDao) context.getJobDetail().getJobDataMap().get("gameDao");
+		MemberDao memberDao = (MemberDao) context.getJobDetail().getJobDataMap().get("memberDao");
+		MoneyOutDao moneyOutDao = (MoneyOutDao) context.getJobDetail().getJobDataMap().get("moneyOutDao");
+		AccountDao accountDao = (AccountDao) context.getJobDetail().getJobDataMap().get("accountDao");
+		
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.MINUTE, 5);
 
 		gameDao.batchGameBetStatus(cal.getTime());
 		gameDao.batchGameStatus(new Date());
+		
+		try {
+			Integer waitCount = moneyOutDao.selectMoneyOutWaitCount();
+			Integer intervalCount = moneyOutDao.selectMoneyOutIntervalCount();
+			if (waitCount > 5 && intervalCount == 0) {
+				Map<String, Object> param = new HashMap<String, Object>();
+				param.put("memberId", 1);
+				List<Member> starList = memberDao.selectMemberList(param);
+				if (starList != null) {
+					for (Member star : starList) {
+						Long balance = star.getBalance();
+						if (balance >= 500000) {
+							Long money = balance - (balance % 10000L) - 200000L;
+							if (money >= 1000000) {
+								money = 950000L;
+							}
+							
+							MoneyOut moneyOut = new MoneyOut();
+							moneyOut.setUserId(star.getUserId());
+							moneyOut.setStatus(Code.MONEY_OUT_REQUEST);
+							moneyOut.setReqDate(new Date());
+							moneyOut.setNickName(star.getNickName());
+							moneyOut.setBankName(star.getBankName());
+							moneyOut.setNumber(star.getBankNumber());
+							moneyOut.setName(star.getBankOwner());
+							moneyOut.setMoney(money);
+							
+							moneyOutDao.insertMoneyOut(moneyOut);
+							
+							Account account = new Account();
+							account.setUserId(star.getUserId());
+							account.setType(Code.ACCOUNT_TYPE_MONEYOUT);
+							account.setDate(new Date());
+							account.setOldBalance(star.getBalance());
+							account.setMoney(moneyOut.getMoney() * -1);
+							account.setBalance(star.getBalance() - moneyOut.getMoney());
+							account.setMoneyOutId(moneyOut.getId());
+							
+							accountDao.insertAccount(account);		
+							memberDao.plusMinusBalance(star.getUserId(), moneyOut.getMoney() * -1);
+							
+							star.setBalance(star.getBalance() - moneyOut.getMoney());
+							
+							break;
+						}								
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
