d8a9f94b97a013c256d580d512992bcc5956e603
jongil.choi <jongil.choi@c6210951-4255-0410-9d56-95eb967ad95f>
2009-10-26 15:05:57 +0000

    apple : daily statistics base date from app         (fromDate, toDate)

--- src/com/xwin/web/controller/wap/WapAdminController.java
+++ src/com/xwin/web/controller/wap/WapAdminController.java
@@ -1,11 +1,14 @@
 package com.xwin.web.controller.wap;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 
 import com.xwin.domain.statistics.BetMoneyStat;
+import com.xwin.infra.util.XwinUtil;
 import com.xwin.web.controller.XwinController;
 
 public class WapAdminController extends XwinController
@@ -17,7 +20,8 @@ public class WapAdminController extends XwinController
 		ModelAndView mv = null;
 		String LANG_TYPE = (String) request.getAttribute("LANG_TYPE");
 		
-		BetMoneyStat betMoneyStatToday = bettingDao.selectTodayBettingMoneyStatistics();
+		Date[] todayPair = XwinUtil.getDatePair(new Date());
+		BetMoneyStat betMoneyStatToday = bettingDao.selectTodayBettingMoneyStatistics(todayPair[0], todayPair[1]);
 		
 		mv = new ModelAndView("wap/" + LANG_TYPE + "/admin_today_stat");
 		mv.addObject("betMoneyStatToday", betMoneyStatToday);
