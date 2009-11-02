1a29d4eaebf90d74695a7c5758d0696355831dbf
jongil.choi <jongil.choi@c6210951-4255-0410-9d56-95eb967ad95f>
2009-11-02 06:07:50 +0000

    apple : sync admin with love

---
+++ src/com/xwin/infra/dao/TotoDao.java
@@ -0,0 +1,50 @@
+package com.xwin.infra.dao;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.xwin.domain.game.Toto;
+
+public class TotoDao extends XwinDao
+{
+	private static final long serialVersionUID = 658511875748204702L;
+
+	public void insertToto(Toto toto)
+	{
+		sqlMapClientTemplate.insert("insertToto", toto);
+	}
+	
+	public void updateToto(Toto toto)
+	{
+		sqlMapClientTemplate.update("updateToto", toto);
+	}
+
+	public Toto selectTotoById(String id) {
+		Map<String, Object> param = new HashMap<String, Object>(1);
+		param.put("id", id);
+		return (Toto) sqlMapClientTemplate.queryForObject("selectToto", param);
+	}
+	
+	public Toto selectToto(Map<String, Object> param)
+	{
+		return (Toto) sqlMapClientTemplate.queryForObject("selectToto", param);
+	}
+	
+	
+	public List<Toto> selectTotoList(Map<String, Object> param)
+	{
+		return sqlMapClientTemplate.queryForList("selectTotoList", param);
+	}
+	
+	public Integer selectTotoCount(Map<String, Object> param)
+	{
+		return (Integer) sqlMapClientTemplate.queryForObject("selectTotoCount", param);
+	}
+	
+	public void batchTotoBetStatus(Date date)
+	{
+		sqlMapClientTemplate.update("batchTotoBetStatus", date);
+	}
+}
